ac6f8990a8d5d0077cab280eadb19dda88672ec3
levidoc <levidoc00@gmail.com>
2026-07-30 09:36:26 +0200

    aDDING tHE bASE AND PAGE FUNCTION

--- collection/hastings.ego/data/head.php
+++ collection/hastings.ego/data/head.php
@@ -18,6 +18,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hastings Ego</title>
+
+    <base href="https://themes.varsitymarket.co.za/collection/hastings.ego/" target="_blank">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
