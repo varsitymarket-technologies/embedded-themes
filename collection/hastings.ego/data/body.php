6f927edfc0ee181ce75ddfc0ff466d7fa9327c49
levidoc <levidoc00@gmail.com>
2026-07-30 10:42:26 +0200

    Added The Pages Functions

--- collection/hastings.ego/data/body.php
+++ collection/hastings.ego/data/body.php
@@ -8,14 +8,14 @@
 
     <footer class="main-footer" style="border:none;">
         <nav class="main-nav" style="margin-bottom: 20px;">
-            <a href="/about/">About Us</a>
-            <a href="/contact/">Contact</a>
-            <a href="/faq/">FAQ</a>
-            <a href="/terms/">Terms</a>
+            <a href="<!-- #!/engine/node/ page('about') -->">About Us</a>
+            <a href="<!-- #!/engine/node/ page('contact') -->">Contact</a>
+            <a href="<!-- #!/engine/node/ page('faq') -->">FAQ</a>
+            <a href="<!-- #!/engine/node/ page('terms') -->">Terms</a>
         </nav>
         <br>
         <p>Powered By Varsity Market Technologies</p>
     </footer>
 </body>
-e(__SYSTEM_ANALYTICS__)
+<!-- #!/engine/node/ analytics() --> 
 </html>
